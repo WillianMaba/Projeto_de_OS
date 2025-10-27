92d51b12609fb958a9331b986228e4543388ef9e
willian <willianrelara@gmail.com>
2025-10-27 15:48:22 -0300

    finalização do esboço inicial do projeto, com todo o funcionamento porem sem def e sem armazenamento de dados, isso será feito em seguida

--- OS/Interface/__init__.py
+++ OS/Interface/__init__.py
@@ -1,6 +1,16 @@
 def linha(tam = 50):
     return '-' * 50
 
+def pergunta_continuar(msg='Deseja continuar? [S/N]: '):
+    while True:
+        resposta = input(msg).strip().lower()
 
+        # Normaliza palavras mais longas
+        if resposta in ['s', 'sim']:
+            return 's'
+        elif resposta in ['n', 'nao', 'não']:
+            return 'n'
+        else:
+            print('\033[0;31mDigite apenas S, N, Sim ou Não!\033[m')
 
 
